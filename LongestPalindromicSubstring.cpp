67c43e6a060285f930fa263eb4b780b325a755fb
Riddance <yixiongx@gmail.com>
2013-12-08 04:50:12 +0800

    add solution
    add an O(1) time solution

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -124,8 +124,8 @@ public:
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int longest_pos = 0;
-        int longest_size = 0;
+        int pos_start = 0;
+        int str_size = 0;
 
         bool is_palindrome[1000][1000] = {false};
 
@@ -151,15 +151,116 @@ public:
                     is_palindrome[j][j + i] = is_palindrome[j + 1][j + i - 1] && (s[j] == s[j + i]);
                 }
 
-                if (i + 1 > longest_size && is_palindrome[j][j + i])
+                if (i + 1 > str_size && is_palindrome[j][j + i])
                 {
-                    longest_pos = j;
-                    longest_size = i + 1;
+                    pos_start = j;
+                    str_size = i + 1;
                 }
 
             }
         }
         
-        return string(s, longest_pos, longest_size);
+        return string(s, pos_start, str_size);
     }
+};
+
+//O(N2) Time and O(1) Space Solution
+class Solution {
+public:
+    string longestPalindrome(string s) {
+
+        string longest_palindrome;
+
+        for (size_t i = 0; i < s.size(); ++i)
+        {
+            string cur_palindrome = get_expand_palindrome_str(s, i, i);
+
+            if (cur_palindrome.size() > longest_palindrome.size())
+            {
+                longest_palindrome = cur_palindrome;
+            }
+
+            cur_palindrome = get_expand_palindrome_str(s, i, i + 1);
+
+            if (cur_palindrome.size() > longest_palindrome.size())
+            {
+                longest_palindrome = cur_palindrome;
+            }
+        }
+
+        return longest_palindrome;
+    }
+
+    string get_expand_palindrome_str(string s, size_t l, size_t r)
+    {
+        size_t s_size = s.size();
+
+        while (l >= 0 && r < s.size() && l <= r && s[l] == s[r])
+        {
+            --l;
+            ++r;
+        }
+
+        return string(s, l + 1, r - 1 - l);
+    }
+};
+
+//O(1) Time Solution
+class Solution
+{
+public:
+    string longestPalindrome(string s) {
+        
+        string pad_string = padding_string(s);
+
+        int *p = new int[pad_string.size()];
+
+        int center = 0;
+        int rhs_edge = 0;
+
+        for (int i = 0; i < (int)pad_string.size(); ++i)
+        {
+            int mirror_i = center * 2 - i;
+
+            p[i] = rhs_edge  > i ? min(p[mirror_i], rhs_edge - i) : 0;
+
+            while ( i - p[i] - 1 >= 0 && (pad_string[i - p[i] - 1] == pad_string[i + p[i] + 1]))
+                p[i]++;
+
+            if (p[i] + i > rhs_edge)
+            {
+                center = i;
+                rhs_edge = p[i] + i;
+            }
+        }
+
+        int palin_size = 0;
+        int index = 0;
+
+        for (int i = 0; i < (int)pad_string.size(); ++i)
+        {
+            if (p[i] > palin_size)
+            {
+                palin_size = p[i];
+                index = i;
+            }
+        }
+
+        delete [] p;
+
+        return string(s, (index - palin_size) / 2, palin_size);
+    }
+
+    string padding_string(string s)
+    {
+        string pad_string;
+
+        for (int i = 0; i < (int)s.size(); ++i)
+        {
+            pad_string = pad_string + "#" + s[i];
+        }
+
+        return pad_string += "#";
+    }
+
 };
