4858e474370ea8566e487ff7fb9b9bf0af2ac4b2
Riddance <yixiongx@gmail.com>
2013-12-09 22:36:25 +0800

    length of longest substring

--- LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -8,8 +8,38 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    	size_t lhs = 0;
-    	size_t rhs = 0;
+
+    	int lss = 0;
+
+    	int str_size = (int)s.size();
+    	int lhs = 0;
+    	int rhs = 0;
+
+    	if (str_size <= 1)
+    		return str_size;
+
+    	while (rhs + 1 <= string_size - 1)
+    	{
+    		int cur_length = 0;
+
+    		asset(lhs <= rhs);
+
+    		string cur_string(s, lhs, rhs - lhs + 1);
+    		size_t find_pos = cur_string.find(s[rhs + 1])
+
+    		if (find_pos == std::string::npos)
+    			++rhs;
+    		else
+    		{
+    			cur_length = rhs - lhs + 1;
+    			lhs = find_pos + 1;
+    		}
+
+    		if (cur_length > lss)
+    			lss = cur_length;
+    	}
         
+
+        return lss;
     }
 };
