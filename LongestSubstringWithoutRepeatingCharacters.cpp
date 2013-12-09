6d8aaf9b8d536fb7090e67c25cf21a759d5033c3
Riddance <yixiongx@gmail.com>
2013-12-10 01:25:12 +0800

    fix bugs(longest substring without repeating character)

--- LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -10,35 +10,38 @@ public:
     int lengthOfLongestSubstring(string s) {
 
     	int lss = 0;
+        int lhs = 0;
+        int rhs = 0;
 
     	int str_size = (int)s.size();
-    	int lhs = 0;
-    	int rhs = 0;
 
     	if (str_size <= 1)
     		return str_size;
 
-    	while (rhs + 1 <= string_size - 1)
+    	while (rhs + 1 <= str_size - 1)
     	{
-    		int cur_length = 0;
+    		int length = 0;
 
-    		asset(lhs <= rhs);
+    		assert(lhs <= rhs);
 
-    		string cur_string(s, lhs, rhs - lhs + 1);
-    		size_t find_pos = cur_string.find(s[rhs + 1])
+            size_t pos = s.find(s[rhs + 1], lhs);
 
-    		if (find_pos == std::string::npos)
-    			++rhs;
-    		else
-    		{
-    			cur_length = rhs - lhs + 1;
-    			lhs = find_pos + 1;
-    		}
+            assert (pos <= rhs + 1);
 
-    		if (cur_length > lss)
-    			lss = cur_length;
+            if (pos != rhs + 1)
+            {
+                length = rhs - lhs + 1;
+                lhs = pos + 1;
+            }
+
+    		if (length > lss)
+    			lss = length;
+
+            ++rhs;
     	}
         
+        if (rhs - lhs + 1 > lss)
+            lss = rhs - lhs + 1;
 
         return lss;
     }
