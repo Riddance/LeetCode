2a378b02dc337505fc8316d211ea7b344df02411
Riddance <yixiongx@gmail.com>
2013-12-25 00:25:27 +0800

    Substring with Concatenation of All Words
    I must sleep now…

---
+++ SubstringwithConcatenationofAllWords.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findSubstring(string S, vector<string> &L) {
+    	std::vector<int> indices;
+
+    	size_t cur_pos = 0;
+    	while (cur_pos < S.size())
+    	{
+    		int indice_begin = 0;
+    		int indice_end = 0;
+
+    		std::vector<string>::iterator Iter = L.begin();
+    		while (Iter != L.end())
+    		{
+
+
+    			++Iter;
+    		}
+    	}
+        
+    }
+};
