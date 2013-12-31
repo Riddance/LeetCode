45679d077fb0b585e037aa740000a71e81479343
Riddance <yixiongx@gmail.com>
2013-12-31 22:51:04 +0800

    Search In Rotated Sorted Array

--- SearchInRotatedSortedArray.cpp
+++ SearchInRotatedSortedArray.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int search(int A[], int n, int target) {
-        
+    	int mid = 0;
+    	int low = 0;
+    	int high = n - 1;
+
+    	while (low <= high) {
+    		mid = (low + high) / 2;
+    		if (A[mid] == target) {
+    			return mid;
+    		}
+    		else if ((A[mid] > target && target >= A[low]) ||
+    		(A[mid] < target && A[high] < target)) {
+    	}
+
+    	return -1;
     }
 };
