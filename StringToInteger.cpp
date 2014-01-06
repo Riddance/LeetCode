952526b62c26a9ed20f9e05ed5c86e37c75c8e19
Riddance <yixiongx@gmail.com>
2014-01-07 00:51:10 +0800

    String To Integer

--- StringToInteger.cpp
+++ StringToInteger.cpp
@@ -8,14 +8,53 @@
 class Solution {
 public:
     int atoi(const char *str) {
-    	int integer = 0;
-    	int flag = 0;
+        int integer = 0;
+        int flag = 0;
 
-    	size_t pos = 0;
-    	while (pos < str.size())
-        {
-            
+        int str_size = (int)strlen(str);
+        int i = 0;
+
+        while (i < str_size) {
+            if (isblank(str[i]) && !flag) {
+                ++i;
+                continue;
+            }
+
+            if (str[i] == '+' && !flag) {
+                ++i;
+                flag = 1;
+                continue;
+            }
+
+            if (str[i] == '-' && !flag) {
+                ++i;
+                flag = -1;
+                continue;
+            }
+
+            if (!flag)
+                flag = 1;
+
+            if (isdigit(str[i]) && flag) {
+                if (flag == 1 && (INT_MAX - (str[i] - '0')) / 10 < integer) {
+                    integer = INT_MAX;
+                    goto out;
+                }
+
+                if (flag == -1 && (INT_MIN + (str[i] - '0')) / 10 > integer * (-1)) {
+                    integer = INT_MIN;
+                    goto out;
+                }
+
+                integer = integer * 10 + (str[i] - '0');
+                ++i;
+                continue;
+            }
+
+            break;
         }
+
+        integer = integer * flag;
 
         out:
         return integer;
