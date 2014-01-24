8fd74998837a96bca36205a7f0c2dc8d9120316c
Riddance <yixiongx@gmail.com>
2014-01-25 00:23:29 +0800

    Construct Binary Tree from Preorder and Inorder Traversal
    Construct Binary Tree from Preorder and Inorder Traversal

--- ConstructBinaryTreefromPreorderandInorderTraversal.cpp
+++ ConstructBinaryTreefromPreorderandInorderTraversal.cpp
@@ -8,23 +8,45 @@
  
 class Solution {
 public:
-    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-    	if (preorder.empty())
-    		return NULL;
+  struct Range {
+    int lhs;
+    int rhs;
+    Range(int x = 0, int y = 0) : lhs(x), rhs(y) {}
+  };
 
-    	TreeNode *cur = new TreeNode;
-    	if (cur) {
-    		std::vector<int> lhs_preorder;
-    		std::vector<int> lhs_inorder;
-    		std::vector<int> rhs_preorder;
-    		std::vector<int> rhs_inorder;
+  TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+    return makeTree(preorder, inorder, Range(0, preorder.size() - 1), Range(0, inorder.size() - 1));
+  }
 
-    		cur->val = *preorder.begin();
+  TreeNode *makeTree(std::vector<int> &pre, std::vector<int> &in, Range raPre, Range raIn) {
+    if (raPre.lhs > raPre.rhs || raIn.lhs > raIn.rhs)
+      return NULL;
 
-    		cur->left = buildTree(lhs_preorder, lhs_inorder);
-    		cur->right = buildTree(rhs_preorder, rhs_inorder);
-    	}
+    if (raPre.lhs < 0 || raPre.rhs >= pre.size() || raIn.lhs < 0 || raIn.rhs >= in.size())
+      return NULL;
 
-        return cur;
+    TreeNode *node = new TreeNode(pre[raPre.lhs]);
+    if (node) {
+      int i = raIn.lhs;
+      while (i <= raIn.rhs) {
+        if (in[i] == pre[raPre.lhs]) {
+          break;
+        }
+        ++i;
+      }
+      assert(i <= raIn.rhs);
+      int ls = i - raIn.lhs;
+      int rs = raIn.rhs - i;
+
+      if (ls) {
+        node->left = makeTree(pre, in, Range(raPre.lhs + 1, raPre.lhs + ls), Range(raIn.lhs, i - 1));
+      }
+
+      if (rs) {
+        node->right = makeTree(pre, in, Range(raPre.rhs - rs + 1, raPre.rhs), Range(i + 1, raIn.rhs));
+      }
     }
+
+    return node;
+  }
 };
