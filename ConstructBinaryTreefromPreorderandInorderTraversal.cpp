031852edd76c65c053f89cdeece4a10387121112
Riddance <yixiongx@gmail.com>
2014-01-24 23:12:30 +0800

    Construct Binary Tree from Preorder and Inorder Traversal

--- ConstructBinaryTreefromPreorderandInorderTraversal.cpp
+++ ConstructBinaryTreefromPreorderandInorderTraversal.cpp
@@ -9,6 +9,22 @@
 class Solution {
 public:
     TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-        
+    	if (preorder.empty())
+    		return NULL;
+
+    	TreeNode *cur = new TreeNode;
+    	if (cur) {
+    		std::vector<int> lhs_preorder;
+    		std::vector<int> lhs_inorder;
+    		std::vector<int> rhs_preorder;
+    		std::vector<int> rhs_inorder;
+
+    		cur->val = *preorder.begin();
+
+    		cur->left = buildTree(lhs_preorder, lhs_inorder);
+    		cur->right = buildTree(rhs_preorder, rhs_inorder);
+    	}
+
+        return cur;
     }
 };
