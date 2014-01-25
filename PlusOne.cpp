b020f1c5bb97e4cfe796622ab8108b5df2c5f6cd
Riddance <yixiongx@gmail.com>
2014-01-25 11:00:40 +0800

    plus one
    plus one

---
+++ PlusOne.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int flag = 1;
+        if (digits.size() == 0)
+        	goto out;
+
+        for (int i = digits.size() - 1; i >= 0; --i) {
+        	int q = (digits[i] + flag) / 10;
+        	int m = (digits[i] + flag) % 10;
+        	digits[i] = m;
+        	flag = q;
+        }
+
+        if (flag) {
+        	digits.insert(digits.begin(), flag);
+        }
+
+        out:
+        return digits;
+    }
+};
