55b44e85a077ff92ced8bc3b003b3fc358e29070
Riddance <yixiongx@gmail.com>
2014-02-07 21:42:13 +0800

    Minimum Path Sum

---
+++ MinimumPathSum.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int> > &grid) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        
+    }
+};
---
+++ ValidNumber.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isNumber(const char *s) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+
+        //trim string
+        while (*s == ' ') {
+        	++s;
+        }
+
+        int len = (int)strlen(s);
+        while (len && *(s + len - 1) == ' ') {
+        	--len;
+        }
+
+        if (!len) {
+        	return false;
+        }
+
+        bool s = false;
+        bool d = false;
+
+        for (int i = 0; i < len; ++i) {
+        	char c = *(s + i);
+        	if (isdigit(c)) {
+        		d = true;
+        	}
+        	else if (c == '.' || c == 'e') {
+        		if (s == true) {
+        			return false;
+        		}
+        		s = true;
+        	}
+        	else if (c == '+' || c == '-') {
+        		if (i != 0 || len == 1) {
+        			return false;
+        		}
+        	}
+        	else {
+        		return false;
+        	}
+        }
+
+        if (!d)
+        	return false;
+
+        return true;
+    }
+};
