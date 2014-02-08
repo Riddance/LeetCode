603a79b2a36536a96334728c0d72d9365424d674
Riddance <yixiongx@gmail.com>
2014-02-09 00:30:03 +0800

    Best Time To Buy and Sell Stock
    Best Time to Buy and Sell Stock

---
+++ BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+    	int l, r;
+    	l = r = 0;
+
+    	if (prices.size() == 0) {
+    		return 0;
+    	}
+
+    	l = prices[0];
+
+    	for (int i = 0; i < (int)prices.size(); ++i) {
+    		if (prices[i] < l) {
+    			l = prices[i];
+    		}
+
+    		int c = prices[i] - l;
+    		if (c > r) {
+    			r = c;
+    		}
+
+    	}
+
+    	return r;
+    }
+};
