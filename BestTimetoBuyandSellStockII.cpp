9b1f93b19d82b44f9ff7ee9c8b0adb8a237b3a51
Riddance <yixiongx@gmail.com>
2014-02-09 16:29:00 +0800

    Unique Binary Search Tree
    Unique Binary Search Tree

---
+++ BestTimetoBuyandSellStockII.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+    	int p = 0;
+    	
+    	return p;
+    }
+};
---
+++ UniqueBinarySearchTrees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numTrees(int n) {
+    	int r = 0;
+
+        if (n == 0)
+        	return 1;
+
+        if (n == 1)
+        	return 1;
+
+        for (int i = 1; i <= n; ++i) {
+        	r = r + numTrees(i - 1)*numTrees(n - i);
+        }
+        
+        return r;
+    }
+};
