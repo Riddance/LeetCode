babee1622a4e756182c91a9b98acee18fc6208f5
Riddance <yixiongx@gmail.com>
2014-02-16 23:25:09 +0800

    ZigZag Conversion
    ZigZag Conversion

--- ZigZagConversion.cpp
+++ ZigZagConversion.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     string convert(string s, int nRows) {
-        
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.  
+        if (nRows <= 1 || s.size() <= 1)  
+        	return s;
+
+        string result;
+        int nCols = s / (nRows + nRows - 2);
+
+        for (int i = 0; i < nRows; ++i) {
+        	for (int j = 0; i < nCols; ++j) {
+        		if (j % (nRows - 1) == 0) {
+        			int nSquence = j / (nRows - 1) * (nRows + nRows - 2) + i;
+        			result.push_back(s[nSquence]);
+        			continue;
+        		}
+
+        		if ((j / (nRows - 1) % 2 == 0 && i == j % (nRows - 1)) ||
+        		(j / (nRows - 1) % 2 != 0 && (i + j % (nRows - 1)) == nRows - 1)) {
+        			int nSquence = 1;
+        			result.push_back(s[nSquence]);
+        			continue;
+        		}
+        	}
+        }
+
+        return result;      
     }
 };
