045cbc0b8080509f44984eb24efc56a7def8a2d6
Riddance <yixiongx@gmail.com>
2014-02-19 10:49:36 +0800

    ZigZag Conversion
    ZigZag Conversion

--- ZigZagConversion.cpp
+++ ZigZagConversion.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    string convert(string s, int nRows) {
+        string convert(string s, int nRows) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.  
         if (nRows <= 1 || s.size() <= 1)  
-        	return s;
+            return s;
 
         string result;
-        int nCols = s.size() / (nRows + nRows - 2) * nRows;
-        if (nCols == 0)
-            nCols = nRows;
+        int nCols = (s.size() / (nRows + nRows - 2) + 1) * nRows;
 
         for (int i = 0; i < nRows; ++i) {
-        	for (int j = 0; j < nCols; ++j) {
+            for (int j = 0; j < nCols; ++j) {
+                int nSquence = -1;
 
-        		if (j % (nRows - 1) == 0) {
-        			int nSquence = j / (nRows - 1) * (nRows + nRows - 2) + i;
-        			if (nSquence < s.size()) {
-        				result.push_back(s[nSquence]);
-        			}
-        			continue;
-        		}
+                if (j % (nRows - 1) == 0) {
+                    nSquence = j / (nRows - 1) * (nRows + nRows - 2) + i;
+                }
+                else if ( (i + j % (nRows - 1)) == nRows - 1) {
+                    nSquence = j / (nRows - 1) * (nRows + nRows - 2) + nRows - 1 + j % (nRows - 1);
+                }
 
-        		if ((j / (nRows - 1) % 2 == 0 && i == j % (nRows - 1)) ||
-        		(j / (nRows - 1) % 2 != 0 && (i + j % (nRows - 1)) == nRows - 1)) {
-        			int nSquence = j / (nRows - 1) * (nRows + nRows - 2) + nRows - 1 + j % (nRows - 1);
-        			if (nSquence < s.size()) {
-        				result.push_back(s[nSquence]);
-        			}
-        			continue;
-        		}
-        	}
+                if (nSquence != -1 && nSquence < (int)s.size()) {
+                    result.push_back(s[nSquence]);
+                }
+            }
         }
-
-        return result;      
+        
+        return result;
     }
 };
