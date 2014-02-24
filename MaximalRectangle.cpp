749aa661d1b29291aca51b9b7799c439bfbc3b3a
Riddance <yixiongx@gmail.com>
2014-02-24 11:05:11 +0800

    Maximal Rectangle
    fix bugs

--- MaximalRectangle.cpp
+++ MaximalRectangle.cpp
@@ -4,62 +4,37 @@ public:
     	if (matrix.size() == 0)
     		return 0;
 
+        int max_area = 0;
+
     	std::vector<int> tmp(matrix[0].size(), 0);
-    	std::vector<std::vector<int> > result(matrix.size(), tmp);
-    	result[0][0] = matrix[0][0] == '1' ? 1 : 0;
-    	int m = result[0][0];
+    	std::vector<std::vector<int> > widths(matrix.size(), tmp);
+
+        for (int i = 0; i < (int)matrix.size(); ++i) {
+            widths[i][0] = matrix[i][0] == '1' ? 1 : 0;
+        }
+
+        for (int i = 0; i < (int)matrix.size(); ++i) {
+            for (int j = 0; j < (int)matrix[0].size(); ++j) {
+                if (j == 0) {
+                    continue;
+                }
+
+                widths[i][j] = matrix[i][j] == '1' ? (widths[i][j - 1] + 1) : 0;
+            }
+        }
 
     	for (int i = 0; i < matrix.size(); ++i) {
     		for (int j = 0; j < matrix[0].size(); ++j) {
-    			if (i == 0 && j == 0) {
-    				continue;
-    			}
+                int height = i;
+                int width_min = widths[i][j];
 
-    			if (i == 0) {
-    				result[i][j] = matrix[i][j] == '1' ? result[i][j - 1] + 1 : 0;
-    				m = max(m, result[i][j]);
-    				continue;
-    			}
-
-    			if (j == 0) {
-    				result[i][j] = matrix[i][j] == '1' ? result[i - 1][j] + 1 : 0;
-    				m = max(m, result[i][j]);
-    				continue;
-    			}
-
-    			if (matrix[i][j] == '0') {
-    				continue;
-    			}
-
-    			if (matrix[i - 1][j] == '0' && matrix[i][j - 1] == '0') {
-    				result[i][j] = 1;
-    				m = max(m, result[i][j]);
-    				continue;
-    			}
-
-    			if (matrix[i - 1][j] == '0') {
-    				result[i][j] = result[i][j - 1] + 1 - result[i - 1][j - 1];
-    				m = max(m, result[i][j]);
-    				continue;
-    			}
-
-    			if (matrix[i][j - 1] == '0') {
-    				result[i][j] = result[i - 1][j] + 1 - result[i - 1][j - 1];
-    				m = max(m, result[i][j]);
-    				continue;
-    			}
-
-    			if (matrix[i - 1][j - 1] == '0') {
-    				result[i][j] = max(result[i - 1][j], result[i][j - 1]) + 1;
-    				m = max(m, result[i][j]);
-    				continue;
-    			}
-
-    			result[i][j] = result[i - 1][j] + result[i][j - 1] - result[i - 1][j - 1] + 1;
-    			m = max(m, result[i][j]);
-    		}
+                while (height >= 0) {
+                    width_min = min(width_min, widths[height][j]);
+                    max_area = max(max_area, width_min * (i - height + 1));
+                    --height;
+                }
     	}
 
-    	return m;
+    	return max_area;
     }
 };
