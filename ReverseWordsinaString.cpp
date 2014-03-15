a63d51b33de8e0f9a86da29e4f175028c4fb252e
Riddance <yixiongx@gmail.com>
2014-03-16 02:40:23 +0800

    Reverse words in a string
    fix bugs

--- ReverseWordsinaString.cpp
+++ ReverseWordsinaString.cpp
@@ -16,24 +16,30 @@ public:
 	    cur = lhs;
 
 	    while (s[cur] != '\0') {
+	    	//assert(rhs - 1 >= lhs)
 	        if (s[cur] != ' ' || (s[cur] == ' ' && s[rhs - 1] != ' ')) {
 	            s[rhs++] = s[cur];
 	        }
 
 	        ++cur;
 	    }
-
+	    
 	    if (s[rhs - 1] == ' ') {
 	        rhs = rhs - 1;
 	    }
 
 	    string tmp(s, lhs, rhs - lhs);
 	    s = tmp;
+	    
+	    if (s.size() == 0) {
+	        return;
+	    }
 
 	    cur = 0;
 	    lhs = 0;
 	    rhs = 0;
-	    //first reverse every word in the string
+
+	    //first, reverse every word in the string
 	    while (s[cur] != '\0') {
 	        if (s[cur] == ' ') {
 	            rhs = cur - 1;
@@ -44,9 +50,21 @@ public:
 	                ++lhs;
 	                --rhs;
 	            }
-	            lhs = rhs = cur + 1;
+	            lhs = cur + 1;
+	            rhs = lhs;
 	        }
-	        cur++;
+	        ++cur;
+	    }
+	    
+	    --cur;
+	    
+	    //reverse the end word
+	    while (lhs < cur) {
+	    	char tmp = s[lhs];
+	    	s[lhs] = s[cur];
+	    	s[cur] = tmp;
+	    	++lhs;
+	    	--cur;
 	    }
 
 	    //now reverse the whole string
