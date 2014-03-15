f6a8270ad076d1cac8160c2b532293140471eaf2
Riddance <yixiongx@gmail.com>
2014-03-16 00:48:16 +0800

    Reverse Words in a String
    Reverse Words in a String

--- BinaryTreeMaximumPathSum.cpp
+++ BinaryTreeMaximumPathSum.cpp
@@ -8,32 +8,8 @@ struct TreeNode {
 class Solution {
 public:
     int maxPathSum(TreeNode *root) {
-    	if (!root)
-    		return 0;
-
-    	int max_path_sum = 0;
-
-    	std::stack<TreeNode*> node_stack;
-    	node_stack.push(root);
-
-    	while (node_stack.empty() == false) {
-    		TreeNode* node = node_stack.top();
-    		node_stack.pop();
-
-    		max_path_sum = max(max_path_sum, node->val + getMaxPathFromRootToLeaf(node->left) + getMaxPathFromRootToLeaf(node->right));
-    		
-    		if (node->left)
-    			node_stack.push(node->left);
-
-    		if (node->right)
-    			node_stack.push(node->right);
-    	}
     }
 
-    int getMaxPathFromRootToLeaf(TreeNode *root) {
-    	if (!root)
-    		return 0;
-
-    	return root->val + max(getMaxPathFromRootToLeaf(root->left), getMaxPathFromRootToLeaf(root->right));
+    int getMaxPathSum(TreeNode* root, int& MaxSingleSum) {
     }
 };
