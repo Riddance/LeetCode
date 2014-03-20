4ee89bc5782054896a865f36096bd5434ae75db7
Riddance <yixiongx@gmail.com>
2014-03-21 00:37:14 +0800

    word break
    will time limit exceeded

---
+++ WordBreak.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string> &dict) {
+
+    	if (s.size() == 0) {
+    			return true;
+    	}
+
+    	for (int i = s.size(); i > 0; --i) {
+    		if (dict.find(s.substr(0, i)) != dict.end()) {
+    			if (wordBreak(s.substr(i, s.size() - i), dict)) {
+    				return true;
+    			}
+    		}
+    	}
+
+    	return false;
+    }
+};
