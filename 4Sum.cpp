ee41b03e9436ef6562cbc9d7c17f21b0a9aa0f7c
Riddance <yixiongx@gmail.com>
2014-03-27 01:47:33 +0800

    4 Sum
    fix bugs

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -1,3 +1,4 @@
+//will TLE
 class Solution {
 public:
     vector<vector<int> > fourSum(vector<int> &num, int target) {
@@ -59,7 +60,6 @@ public:
     				continue;
     			}
 
-
     			r[0] = num[lhs->second.index1];
     			r[1] = num[lhs->second.index2];
     			r[2] = num[rhs->second.index1];
@@ -70,6 +70,65 @@ public:
 				if (std::find(rv.begin(), rv.end(), r) == rv.end()) {
 					rv.push_back(r);
                 }
+    		}
+    	}
+
+    	return rv;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int> > fourSum(vector<int> &num, int target) {
+    	std::vector<std::vector<int> > rv;
+    	std::vector<int> r(4, 0);
+
+    	if (num.size() < 4) {
+    		return rv;
+    	}
+
+    	std::sort(num.begin(), num.end(), std::less<int>());
+
+    	for (int i = 0; i < (int)num.size() - 1; ++i) {
+    	    
+    	    if (i != 0 && num[i] == num[i - 1]) {
+    	        continue;
+    	    }
+    	    
+    		for (int j = i + 1; j < (int)num.size(); ++j) {
+    			int lhs = j + 1;
+    			int rhs = num.size() - 1;
+
+    			if (j != i + 1 && num[j] == num[j - 1]) {
+    				continue;
+    			}
+
+    			while (lhs < rhs) {
+    			    
+    			    if (lhs != j + 1 && num[lhs] == num[lhs - 1]) {
+    			        ++lhs;
+    			        continue;
+    			    }
+    			    
+    				int sum4 = num[i] + num[j] + num[lhs] + num[rhs];
+    				if (sum4 == target) {
+    					r[0] = num[i];
+    					r[1] = num[j];
+    					r[2] = num[lhs];
+    					r[3] = num[rhs];
+
+    					rv.push_back(r);
+
+    					++lhs;
+    					--rhs;
+    				}
+    				else if (sum4 < target) {
+    					++lhs;
+    				}
+    				else {
+    					--rhs;
+    				}
+    			}
     		}
     	}
 
