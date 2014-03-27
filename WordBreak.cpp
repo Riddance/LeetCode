dce9fe54f5b4e1b8d9f54c993302661fc72632d9
Riddance <yixiongx@gmail.com>
2014-03-27 16:43:50 +0800

    Word Break
    Word Break

--- WordBreak.cpp
+++ WordBreak.cpp
@@ -1,12 +1,13 @@
+//will TLE
 class Solution {
 public:
     bool wordBreak(string s, unordered_set<string> &dict) {
 
-        if (s.size() == 0) {
+        if ((int)s.size() == 0) {
                 return true;
         }
 
-        for (int i = s.size(); i > 0; --i) {
+        for (int i = (int)s.size(); i > 0; --i) {
             if (dict.find(s.substr(0, i)) != dict.end()) {
                 if (wordBreak(s.substr(i, s.size() - i), dict)) {
                     return true;
@@ -17,3 +18,47 @@ public:
         return false;
     }
 };
+
+//use a unorderd_map to recorder result
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string> &dict) {
+        if (s.size() == 0 && dict.find("") == dict.end()) {
+            return false;
+        }
+
+        std::unordered_map<std::string, bool> strToBoolMap;
+        return _wordBreak(s, dict, strToBoolMap);
+    }
+
+
+    bool _wordBreak(
+        std::string s,
+        std::unordered_set<std::string> &dict,
+        std::unordered_map<std::string, bool> &strToBoolMap
+    ) {
+        if (s.size() == 0) {
+            return true;
+        }
+
+        for (int i = 1; i <= (int)s.size(); ++i) {
+            if (dict.find(s.substr(0, i)) == dict.end()) {
+                continue;
+            }
+
+            assert(s.size() >= i);
+            std::string suffixStr = s.substr(i, s.size() - i);
+
+            if (strToBoolMap.find(suffixStr) == strToBoolMap.end()) {
+                strToBoolMap[suffixStr] = _wordBreak(suffixStr, dict, strToBoolMap);
+            }
+
+            if (strToBoolMap[suffixStr]) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
+
