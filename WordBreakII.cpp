aceb4988d01b11662b2dcae080694a368116bfef
Riddance <yixiongx@gmail.com>
2014-03-27 20:04:48 +0800

    Word Break 2
    Word Break 2

---
+++ WordBreakII.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<string> wordBreak(string s, unordered_set<string> &dict) {
+    	std::vector<string> resultStr;
+
+    	if (s.size() == 0 && dict.find("") == dict.end()) {
+    		return false;
+    	}
+
+
+    	return resultStr;
+    }
+};
