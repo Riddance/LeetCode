771a7ea6caecf524fbcd2d0164371d9003ae4aa5
Riddance <yixiongx@gmail.com>
2014-04-01 23:06:48 +0800

    Word Break 2
    Word Break 2

--- WordBreakII.cpp
+++ WordBreakII.cpp
@@ -1,14 +1,48 @@
 class Solution {
 public:
     vector<string> wordBreak(string s, unordered_set<string> &dict) {
-    	std::vector<string> resultStr;
 
-    	if (s.size() == 0 && dict.find("") == dict.end()) {
-    		return resultStr;
+        if (s.size() == 0 && dict.find("") == dict.end()) {
+            return false;
+        }
+
+        unordered_map<string, vector<string> > strToWordsMap;
+
+    	return _wordBreak(s, dict, strToWordsMap);
+    }
+
+    vector<string> _wordBreak(
+    	string s,
+    	unordered_set<string> &dict,
+    	unordered_map<string, vector<string> > &strToWordsMap
+    ) {
+
+    	vector<string> vecStr;
+
+    	if (s.size() == 0) {
+    		vecStr.push_back("");
+    		return vecStr;
     	}
 
-    	
+    	for (int i = 1; i < (int)s.size(); ++i) {
+    		string lhsStr = s.substr(0, i);
+    		string rhsStr = s.substr(i, s.size() - 1);
 
-    	return resultStr;
+    		if (dict.find(lhsStr) == dict.end()) {
+    			continue;
+    		}
+
+    		if (strToWordsMap.find(rhsStr) == strToWordsMap.end()) {
+    			strToWordsMap[rhsStr] = _wordBreak(rhsStr, dict, strToWordsMap);
+    		}
+
+    		if ((strToWordsMap[rhsStr]).empty() == false) {
+    			vecStr = strToWordsMap[rhsStr];
+    			vecStr.push_back(lhsStr);
+    			return vecStr;
+    		}
+    	}
+
+    	return vecStr;
     }
 };
