6e00e4110a9e9112f8027964006d083a61e29d15
Riddance <yixiongx@gmail.com>
2014-04-29 01:18:48 +0800

    Sudoku Solver
    Sudoku Solver

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void solveSudoku(vector<vector<char> > &board) {
+
         int row = board.size();
         if (row != 9) {
             return;
@@ -11,7 +12,37 @@ public:
             return;
         }
 
-        vector<vector<int> > vecNum(row, vector<int>(col, 0));
-        
+        vector<vector<int> > vecNum(row, vector<int>(col, 0x3FE));
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+
+                int val = (int)(board[i][j] - '0');
+
+                for (int k = 0; k < row; ++k) {
+                    vecNum[k][j] &= ~(1 << val);
+                }
+
+                for (int k = 0; k < col; ++k) {
+                    vecNum[i][k] &= ~(1 << val);
+                }
+
+                int celRow = i / 3;
+                int celCol = j / 3;
+
+                for (int m = celRow; m < celRow + 3; ++m) {
+                    for (int n = celCol; n < celCol + 3; ++n) {
+                        vecNum[m][n] &= ~(1 << val);
+                    }
+                }
+
+                vecNum[i][j] = 1 << val;
+            }
+        }
+
+        multimap
+
     }
 };
