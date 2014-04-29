77f30bec309d378801b8fb57ef822de37b03bb26
Riddance <yixiongx@gmail.com>
2014-04-29 23:30:43 +0800

    Sudoku Solver
    Sudoku Solver

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -1,48 +1,74 @@
 class Solution {
 public:
     void solveSudoku(vector<vector<char> > &board) {
+        dfsSolveSudoku(board);
+        return;
+    }
 
+    bool dfsSolveSudoku(vector<vector<char> > &board) {
         int row = board.size();
+
         if (row != 9) {
-            return;
+            return false;
         }
 
         int col = board[0].size();
         if (col != 9) {
-            return;
+            return false;
         }
 
-        vector<vector<int> > vecNum(row, vector<int>(col, 0x3FE));
         for (int i = 0; i < row; ++i) {
             for (int j = 0; j < col; ++j) {
                 if (board[i][j] == '.') {
-                    continue;
-                }
+                    for (int k = 1; k <= 9; ++k) {
+                        board[i][j] = '0' + k;
 
-                int val = (int)(board[i][j] - '0');
+                        if (checkValid(board, i, j)) {
+                            if (dfsSolveSudoku(board)) {
+                                return true;
+                            }
 
-                for (int k = 0; k < row; ++k) {
-                    vecNum[k][j] &= ~(1 << val);
-                }
-
-                for (int k = 0; k < col; ++k) {
-                    vecNum[i][k] &= ~(1 << val);
-                }
-
-                int celRow = i / 3;
-                int celCol = j / 3;
-
-                for (int m = celRow; m < celRow + 3; ++m) {
-                    for (int n = celCol; n < celCol + 3; ++n) {
-                        vecNum[m][n] &= ~(1 << val);
+                            board[i][j] = '.';
+                        }
                     }
-                }
 
-                vecNum[i][j] = 1 << val;
+                    return false;
+                }
             }
         }
 
-        multimap
+        return true;
+    }
 
+    bool checkValid(vector<vector<char> > &board, int x, int y) {
+
+        for (int i = 0; i < board.size(); ++i) {
+            if (i != x && board[i][y] == board[x][y]) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < board[0].size(); ++i) {
+            if (i != y && board[x][i] == board[x][y]) {
+                return false;
+            }
+        }
+
+        int celRow = x / 3 * 3;
+        int celCol = x / 3 * 3;
+
+        for (int i = celRow; i < celRow + 3; ++i) {
+            for (int j = celCol; j < celCol + 3; ++j) {
+                if (i == x && j == y) {
+                    continue;
+                }
+
+                if (board[i][j] == board[x][y]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 };
