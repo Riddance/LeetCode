de2c69823b8a75999396a8de27919a817982ff7c
Riddance <yixiongx@gmail.com>
2014-05-12 00:26:36 +0800

    Distinct Subsequences
    Distinct Subsequences

--- DistinctSubsequences.cpp
+++ DistinctSubsequences.cpp
@@ -1,6 +1,54 @@
 class Solution {
 public:
     int numDistinct(string S, string T) {
-        
+    	int m = S.size();
+    	int n = T.size();
+
+    	if (m < n) {
+    		return 0;
+    	}
+
+    	vector<vector<int> > result(m + 1, vector<int>(n + 1, 0));
+    	for (int i = 0; i <= m; ++i) {
+    		result[i][0] = 1;
+    	}
+
+    	for (int i = 1; i <= m; ++i) {
+    		for (int j = 1; j <= n; ++j) {
+    			result[i][j] = result[i - 1][j];
+    			if (S[i - 1] == T[j - 1]) {
+    				result[i][j] += result[i - 1][j - 1];
+    			}
+    		}
+    	}
+
+    	return result[m][n];
     }
+};
+
+
+//this Solution will TLE
+class Solution {
+public:
+    int numDistinct(string S, string T) {
+    	int num = 0;
+
+    	if (T.size() == 0) {
+    		num = 1;
+    		goto out;
+    	}
+
+    	if (S.size() == 0) {
+    		goto out;
+    	}
+
+    	for (int i = 0; i < S.size(); ++i) {
+    		if (S[i] == T[0]) {
+    			num += numDistinct(S.substr(i + 1), T.substr(1)); 
+    		}
+    	}
+
+    	out:
+    	return num;
+  }
 };
