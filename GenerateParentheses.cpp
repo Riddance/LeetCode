e7f7a05ab082512ce0359f409f70f47743b2e85b
Riddance <yixiongx@gmail.com>
2014-05-13 17:15:49 +0800

    Generate Parentheses
    Generate Parentheses

--- GenerateParentheses.cpp
+++ GenerateParentheses.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-    	std::vector<string> vecStr;
-        
+        vector<string> result;
+
+        helpFunction(n, n, "", result);
+
+        return result;
+    }
+
+    void helpFunction(int leftbracket, int rightbracket, string s, vector<string>& result) {
+        if (leftbracket == 0 && rightbracket == 0) {
+            result.push_back(s);
+            return;
+        }
+
+        if (leftbracket > 0) {
+            helpFunction(leftbracket - 1, rightbracket, s + "(", result);
+        }
+
+        if (rightbracket > 0 && leftbracket < rightbracket) {
+            helpFunction(leftbracket, rightbracket - 1, s + ")", result);
+        }
+
+        return;
     }
 };
