bf5c61599edd4382ada38065ed2030fc6f4ee0a8
Riddance <yixiongx@gmail.com>
2014-05-14 20:57:33 +0800

    fix bugs

--- SortColors.cpp
+++ SortColors.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     void sortColors(int A[], int n) {
 
-    	vector<int> v(3, 0);
+        vector<int> v(3, 0);
 
-    	for (int i = 0; i < n; ++i) {
-    		v[A[i]]++;
-    	}
+        for (int i = 0; i < n; ++i) {
+            v[A[i]]++;
+        }
 
-    	for (int i = 0; i < v[0]; ++i) {
-    		*A++ = 0;
-    	}
+        for (int i = 0; i < v[0]; ++i) {
+            *A++ = 0;
+        }
 
-    	for (int i = 0; i < v[1]; ++i) {
-    		*A++ = 1;
-    	}
+        for (int i = 0; i < v[1]; ++i) {
+            *A++ = 1;
+        }
 
-    	for (int i = 0; i < v[2]; ++i) {
-    		*A++ = 2;
-    	}
+        for (int i = 0; i < v[2]; ++i) {
+            *A++ = 2;
+        }
 
-    	return;
+        return;
     }
 };
 
@@ -28,25 +28,25 @@ class Solution {
 public:
     void sortColors(int A[], int n) {
 
-    	int r = -1;
-    	int g = -1;
-    	int b = -1;
+        int r = -1;
+        int g = -1;
+        int b = -1;
 
-    	for (int i = 0; i < n; ++i) {
-    		if (A[i] == 0) {
-    			A[++b] = 2;
-    			A[++g] = 1;
-    			A[++r] = 0;
-    		}
-    		else if (A[i] == 1) {
-    			A[++b] = 2;
-    			A[++g] = 1;
-    		}
-    		else {
-    			A[++b] = 2;
-    		}
-    	}
+        for (int i = 0; i < n; ++i) {
+            if (A[i] == 0) {
+                A[++b] = 2;
+                A[++g] = 1;
+                A[++r] = 0;
+            }
+            else if (A[i] == 1) {
+                A[++b] = 2;
+                A[++g] = 1;
+            }
+            else {
+                A[++b] = 2;
+            }
+        }
 
-    	return;
+        return;
     }
 };
