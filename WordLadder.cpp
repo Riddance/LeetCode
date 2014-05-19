6efcaac436e0b473a9b6548282b81a26d700cb7e
Riddance <yixiongx@gmail.com>
2014-05-20 00:37:12 +0800

    word ladder
    word ladder

---
+++ WordLadder.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int ladderLength(string start, string end, unordered_set<string> &dict) {
+
+
+    	for (auto it = dict.begin(); it != dict.end(); ++it) {
+    	}
+
+    }
+};
