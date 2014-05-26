316ae72e2ca10c8dfcb8f13fd0de7ba70930a232
Riddance <yixiongx@gmail.com>
2014-05-26 10:17:54 +0800

    Regular Expression Matching
    Regular Expression Matching

---
+++ RegularExpressionMatching.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        if (s == NULL) {
+            return p == NULL;
+        }
+
+        if (p == NULL) {
+            return false;
+        }
+
+        if (*p == '\0') {
+            return *s == '\0';
+        }
+
+        if (*(p + 1) != '*') {
+            return ((*p == *s) || (*p == '.' && *s != '\0')) && isMatch(s + 1, p + 1);
+        }
+
+        while ((*p == *s) || (*p == '.' && *s != '\0')) {
+            if (isMatch(s, p + 2)) {
+                return true;
+            }
+
+            ++s;
+        }
+
+        return isMatch(s, p + 2);
+    }
+};
