7c733ed6c264690a0e930e16ed0202e416952967
Riddance <yixiongx@gmail.com>
2014-05-27 22:15:07 +0800

    Fix Bugs
    Fix Bugs

--- RegularExpressionMatching.cpp
+++ RegularExpressionMatching.cpp
@@ -1,126 +1,49 @@
 class Solution {
 public:
-    bool getMatchStr(const char *p, int c, int &l, int &r) {
-        if (p[c] == '\0') {
-            return false;
+    const char* kmpStrStr(const char* s, const char* d) {
+        if (s == nullptr || d == nullptr) {
+            return nullptr;
         }
 
-        while (p[c] == '*') {
-            ++c;
+        int len = (int)strlen(d);
+        if (len == 0) {
+            return s;
         }
 
-        l = c;
+        int j = 0;
+        vector<int> next_j(len + 1, 0);
+        for (int i = 2; i <= len; ++i) {
 
-        while (p[c] != '*' && p[c] != '\0') {
-            ++c;
+            while (j > 0 && d[j] != d[i - 1]) {
+                j = next_j[j];
+            }
+
+            if (d[j] == d[i - 1]) {
+                j = j + 1;
+            }
+
+            next_j[i] = j;
         }
 
-        r = c;
+        j = 0;
+        for (int i = 0; i < (int)strlen(s); ++i) {
+            while (j > 0 && d[j] != s[i] && d[j] != '?') {
+                j = next_j[j];
+            }
 
-        if (r - l < 1) {
-            return false;
+            if (d[j] == s[i] || d[j] == '?') {
+                j = j + 1;
+            }
+
+            if (j == len) {
+                return s + i - len + 1;
+            }
         }
 
-        return true;
+        return nullptr;
     }
 
-    bool isMatch(const char *s, const char *p)
-    {
-        int l = 0;
-        int r = 0;
-        int c = 0;
-
-        if (strlen(p) == 0) {
-            return strlen(s) == 0;
-        }
-
-        while(getMatchStr(p, c, l, r)) {
-            
-            if (l == 0 && r == strlen(p)) {
-                if (strlen(p) != strlen(s)) {
-                    return false;
-                }
-
-                for (int i = 0; i < (int)strlen(s); ++i) {
-                    if (p[i] != s[i] && p[i] != '?') {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-
-            if (l == 0) {
-                for (int i = 0; i < (int)r - (int)l; ++i) {
-                    if (p[i] != s[i] && p[i] != '?') {
-                        return false;
-                    }
-                }
-            }
-
-            if (r == strlen(p)) {
-                int len = strlen(s);
-
-                for (int i = 0; i < (int)r - (int)l; ++i) {
-                    if (p[r - i - 1] != s[len - i - 1] && p[r - i - 1] != '?') {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-
-            string match_str = string(p + l, r - l);
-
-            bool b_match = false;
-
-            //KMP match string
-            int j = 0;
-            vector<int> next_j(match_str.size() + 1, 0);
-            for (int i = 2; i <= (int)match_str.size(); ++i)
-            {
-                while (j > 0 && match_str[j] != match_str[i - 1])
-                {
-                    j = next_j[j];
-                }
-
-                if (match_str[j] == match_str[i - 1])
-                {
-                    j = j + 1;
-                }
-
-                next_j[i] = j;
-            }
-
-            j = 0;
-            for (int i = c; i < (int)strlen(s); ++i)
-            {
-                while (j > 0 && match_str[j] != s[i] && match_str[j] != '?')
-                {
-                    j = next_j[j];
-                }
-
-                if (match_str[j] == s[i] || match_str[j] == '?')
-                {
-                    j = j + 1;
-                }
-
-                if (j == (int)match_str.size())
-                {
-                    c = c + j;
-                    b_match = true;
-                    break;
-                }
-            }
-
-            if (b_match == false)
-            {
-                return false;
-            }
-
-            c = r;
-        }
-
+    bool isMatch(const char *s, const char *p) {
         return true;
     }
 };
