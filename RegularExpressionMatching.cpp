9a345a0ba1109a6c05d9b7b43d0daf72636f790f
Riddance <yixiongx@gmail.com>
2014-05-28 15:32:16 +0800

    Regular Expression Matching
    Regular Expression Matching

--- RegularExpressionMatching.cpp
+++ RegularExpressionMatching.cpp
@@ -14,11 +14,11 @@ public:
         vector<int> next_j(len + 1, 0);
         for (int i = 2; i <= len; ++i) {
 
-            while (j > 0 && d[j] != d[i - 1]) {
+            while (j > 0 && d[j] != d[i - 1] && d[j] != '?' && d[i - 1] != '?') {
                 j = next_j[j];
             }
 
-            if (d[j] == d[i - 1]) {
+            if (d[j] == d[i - 1] || d[j] == '?' || d[i - 1] == '?') {
                 j = j + 1;
             }
 
@@ -43,7 +43,105 @@ public:
         return nullptr;
     }
 
+    bool getMatchStr(const char *p, int pos, int &l , int &r) {
+        if (p[pos] == '\0') {
+            return false;
+        }
+
+        while (p[pos] == '*') {
+            ++pos;
+        }
+
+        l = pos;
+
+        while (p[pos] != '*' && p[pos] != '\0') {
+            ++pos;
+        }
+
+        r = pos - 1;
+
+        if (l > r) {
+            return false;
+        }
+
+        if (l == r && p[l] == '\0') {
+            return false;
+        }
+
+        return true;
+    }
+
     bool isMatch(const char *s, const char *p) {
+        int p_cur = 0;
+        int s_cur = 0;
+
+        int l = 0;
+        int r = 0;
+
+        if (strlen(p) == 0) {
+            return strlen(s) == 0;
+        }
+
+        if (strlen(s) == 0) {
+            return !getMatchStr(p, p_cur, l, r);
+        }
+
+        while (getMatchStr(p, p_cur, l, r)) {
+            p_cur = r + 1;
+
+            if (l == 0 && r == strlen(p) - 1) {
+                if (strlen(s) != strlen(p)) {
+                    return false;
+                }
+
+                if (kmpStrStr(s, p) == nullptr) {
+                    return false;
+                }
+
+                return true;
+            }
+
+            if (l == 0) {
+                if ((int)strlen(s) < r - l + 1) {
+                    return false;
+                }
+
+                for (int i = 0; i < r - l + 1; ++i) {
+                    if (p[i] != s[i] && p[i] != '?') {
+                        return false;
+                    }
+                }
+
+                s_cur = s_cur + r - l + 1;
+                continue;
+            }
+
+            if (r == strlen(p) - 1) {
+                int len_s = (int)strlen(s);
+                int len_p = (int)strlen(p);
+
+                if (len_s - s_cur < r - l + 1) {
+                    return false;
+                }
+
+                for (int i = 0; i < r - l + 1; ++i) {
+                    if (s[len_s - i - 1] != p[len_p - i - 1] && p[len_p - i - 1] != '?') {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            string sub_str = string(p + l, r - l + 1);
+            const char* find_pos = kmpStrStr(s + s_cur, sub_str.c_str());
+            if (find_pos == nullptr) {
+                return false;
+            }
+
+            s_cur = (int)(find_pos - s + r - l + 1);
+        }
+
         return true;
     }
 };
