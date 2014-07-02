911d9e51413bc103b082dc3630631d5daedd4fe7
Riddance <yixiongx@gmail.com>
2014-07-02 21:20:58 +0800

    add solutions
    add solutions

--- BinaryTreeInorderTraversal.cpp
+++ BinaryTreeInorderTraversal.cpp
@@ -28,3 +28,30 @@ public:
         inorder_traver(root->right, result);
     }
 };
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<int> result;
+        std::stack<TreeNode *> walk_stack;
+
+        while (!walk_stack.empty() || root) {
+            if (root) {
+                walk_stack.push(root);
+                root = root->left;
+                continue;
+            }
+
+            if (!walk_stack.empty()) {
+                root = walk_stack.top();
+                walk_stack.pop();
+                result.push_back(root->val);
+                root = root->right;
+                continue;
+            }
+        }
+
+        return result;
+    }
+};
--- BinaryTreePostorderTraversal.cpp
+++ BinaryTreePostorderTraversal.cpp
@@ -31,3 +31,37 @@ public:
         return;
     }
 };
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        vector<int> result;
+        std::stack<TreeNode *> walk_stack;
+        TreeNode * last_visite = NULL;
+
+        while (!walk_stack.empty() || root) {
+            if (root) {
+                walk_stack.push(root);
+                root = root->left;
+                continue;
+            }
+
+            if (!walk_stack.empty()) {
+                if (walk_stack.top()->right == last_visite || walk_stack.top()->right == NULL) {
+                    last_visite = walk_stack.top();
+                    result.push_back(last_visite->val);
+                    walk_stack.pop();
+                }
+                else {
+                    root = walk_stack.top()->right;
+                }
+            }
+        }
+
+        return result;
+    }
+
+};
+
