620afbb4753b5d19e35c4de17b34187b88c1714b
yixiongx <xiongyi@XiongdeMacBook-Air.local>
2015-05-04 21:10:56 +0800

    add solutions
    find minimumin routate sorted array

---
+++ FindMinimuminRotatedSortedArray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int lhs = 0;
+        int rhs = (int)nums.size() - 1;
+
+        while (rhs - lhs > 1) {
+            int mid = (rhs + lhs) / 2;
+
+            if (nums[lhs] <= nums[mid] && nums[mid] <= nums[rhs]) {
+                break;
+            }
+            else if (nums[lhs] > nums[mid]) {
+                rhs = mid;
+            }
+            else {
+                lhs = mid;
+            }
+        }
+
+        return std::min(nums[lhs], nums[rhs]);
+    }
+};
